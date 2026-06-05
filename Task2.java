ce887e021c0f6b343bd6472cc05d4213405fbdc8
ranross <ranross1111@gmail.com>
2026-06-05 13:32:52 +0000

    Task2

---
+++ Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int    h    = 3;
+        char   e    = 'H';
+        byte   zero = 0;
+        short  two  = 2;
+        float  f    = 0f;
+        boolean b   = true;
+        String output = e + "" + h + "ll" + zero + " w" + zero + "rld " + two + "." + (int)f + " " + b;
+        System.out.println(output);
+    }
+}
